7fcf9be60c7a0d1039a35a8b16500bbd16471066
JuanJesuss <90684570+JuanJesuss@users.noreply.github.com>
2022-12-14 10:14:50 +0100

    Update controller-alta-juego.php

--- controllers/controller-alta-juego.php
+++ controllers/controller-alta-juego.php
@@ -14,21 +14,20 @@
                 else{
                     $ids= obtener_id_todos_juegos($db);
                     $mayor= 0;
-                    $ids_juegos= array();
                     while($row= mysqli_fetch_assoc($ids)){
                         if(intval($row['id'])>$mayor)
                             $mayor= intval($row['id']);
                     }
                     $fecha= date('Y-m-d');
-                    $ids_hoy= obtener_id_juegos_hoy($fecha, $db);
+                    $ids_hoy= obtener_id_juegos_hoy($fecha, $_SESSION['turno'], $db);
                     if(mysqli_num_rows($ids_hoy)==0){
-                        if(alta_juego(($mayor+1), strtolower(eliminar_tildes(trim($_POST['nombrejuego']))), $fecha, $db))
-                            echo "<p class=trece>Juego dado de alta con éxito:<p/><p class=catorce>- Id juego: ".($mayor+1)."<br/>- Juego: ".strtolower(eliminar_tildes(trim($_POST['nombrejuego'])))."<br/>- Fecha alta: ".date("d/m/Y", strtotime($fecha))."</p>";
+                        if(alta_juego(($mayor+1), strtolower(eliminar_tildes(trim($_POST['nombrejuego']))), $fecha, $_SESSION['turno'], $db))
+                            echo "<p class=trece>Juego dado de alta con éxito:<p/><p class=catorce>- Id juego: ".($mayor+1)."<br/>- Juego: ".strtolower(eliminar_tildes(trim($_POST['nombrejuego'])))."<br/>- Fecha alta: ".date("d/m/Y", strtotime($fecha))."<br/>- Turno: ".$_SESSION['turno']."</p>";
                     }
                     elseif(mysqli_num_rows($ids_hoy)==5)
                         echo "<span class=once>Solo se puede dar de alta un máximo de 5 juegos por día.</span>";
                     else{
-                        $fila= obtener_juegos($db);
+                        $fila= obtener_juegos($fecha, $_SESSION['turno'], $db);
                         $juegos= array();
                         while($row = mysqli_fetch_assoc($fila)){
                             array_push($juegos, $row['juego']);
@@ -36,8 +35,8 @@
                         if(in_array(strtolower(eliminar_tildes(trim($_POST['nombrejuego']))), $juegos))
                             echo "<span class=once>El juego ".eliminar_tildes(trim($_POST['nombrejuego']))." ya ha sido dado de alta el día de hoy.</span>";
                         else{
-                            if(alta_juego(($mayor+1), strtolower(eliminar_tildes(trim($_POST['nombrejuego']))), $fecha, $db))
-                                echo "<p class=trece>Juego dado de alta con éxito:<p/><p class=catorce>- Id juego: ".($mayor+1)."<br/>- Juego: ".strtolower(eliminar_tildes(trim($_POST['nombrejuego'])))."<br/>- Fecha alta: ".date("d/m/Y", strtotime($fecha))."</p>";
+                            if(alta_juego(($mayor+1), strtolower(eliminar_tildes(trim($_POST['nombrejuego']))), $fecha, $_SESSION['turno'], $db))
+                                echo "<p class=trece>Juego dado de alta con éxito:<p/><p class=catorce>- Id juego: ".($mayor+1)."<br/>- Juego: ".strtolower(eliminar_tildes(trim($_POST['nombrejuego'])))."<br/>- Fecha alta: ".date("d/m/Y", strtotime($fecha))."<br/>- Turno: ".$_SESSION['turno']."</p>";
                         }
                     } 
                 }
@@ -46,3 +45,4 @@
     }
     else
         header("location: controller-sesion.php");
+?>
